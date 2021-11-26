211048c121b46a3a3c5c49c0dfc33426c3b455df
SushanGhimire <sushangmi55@gmail.com>
2021-11-26 15:28:08 +0545

    text removed

--- src/components/website/home/integrate/Join.jsx
+++ src/components/website/home/integrate/Join.jsx
@@ -8,13 +8,13 @@ function Join() {
     <div className="max-width my-10">
       <div className="bg-lightGray px-5 sm:px-10 md:px-16 py-24 rounded-xl">
         <div className="text-darkGray sm:w-100  space-y-3">
-          <header
+          {/* <header
             className="text-paragraph text-light text-left"
             data-aos="fade-up"
             data-aos-duration="1000"
           >
             DOWNLOAD OUR APP
-          </header>
+          </header> */}
           <div
             className="text-5xl font-manrope font-black tracking-wide text-center sm:text-left leading-snug"
             data-aos="fade-up"
