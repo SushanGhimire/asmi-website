aa6d1c2721cda71e86556a28b43ccbfe067d981f
raghibislam <islamraghib3@gmail.com>
2021-11-28 23:49:01 +0545

    Added typeform

--- src/components/website/contact/integrate/TypeForm.jsx
+++ src/components/website/contact/integrate/TypeForm.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { Widget } from '@typeform/embed-react';
 
 function TypeForm() {
-  return <div className="max-width py-20">Type Form Here</div>;
+  return <div className="max-width py-20">
+     <Widget id="Cu4AjA3B" style={{ width: '100%' , height:'500px' }} className="my-form" />
+  </div>;
 }
 
 export default TypeForm;
