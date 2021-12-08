ca125d33be0e00a73e1e7551a98be207dd5754cc
raghibislam <islamraghib3@gmail.com>
2021-12-08 18:14:15 +0545

    changes

--- src/components/website/about/integrate/TeamMembers.jsx
+++ src/components/website/about/integrate/TeamMembers.jsx
@@ -48,137 +48,8 @@ let founders = [
       "Sajjan has built software tools at tremendous scale and managed delivery timelines across team sizes of up to 200. Sajjan served as the CEO of Selcouth Technology alongside Sam, one of Nepal’s earliest and most successful B2B e-commerce platforms. Currently, he leads the engineering division of ASMI and oversees the day-to-day aspects of the company management. Sajjan implement standard operating procedures that provide world-class support and efficiency for ASMI’s customers.",
   },
 ];
-let teams = [
-  {
-    name: "Sagar Adhikari",
-    img: sagar,
-    position: "Engineer Manager/AI Engineer",
-    description:
-      "Hi, this is Sagar. I plan, coordinate and oversee the technical and engineering activities in the organisation facillitating the product development and integration with clients. For fun, I love to read.",
-  },
-  {
-    name: "Sanjib Raj Acharya",
-    img: sanjib,
-    position: "Full Stack - Backend",
-    description:
-      "Hi. My name is Sanjib Acharya. With 4 years of experience in software development, I am working as a Backend Engineer in ASMI while I assist the team with system architecture, data storage and management.",
-  },
-  {
-    name: "Niten Lama",
-    img: niten,
-    position: "AI Engineer",
-    description:
-      "Hi, myself Niten and I've been working as an AI Engineer at ASMI since past 2 years. I have a combined knowledge of Computer Vision and Machine Learning techniques and I mainly enjoy solving vision related challenges, basically anything that requires processing and extracting essential information related to visual data. I love to sing and I'm also into drumming.",
-  },
-  {
-    name: "Gaurab Subedi",
-    img: gaurav,
-    position: "AI Engineer",
-    description:
-      "Hello, I am Gaurab Subedi. I hold a BE degree in computer engineering from Tribhuvan University. In ASMI, I am working as an AI Engineer and Researcher having two years of experience. I have knowledge of machine learning and computer vision and carry out research work for the company. In my free time, I love to travel and listen to music. ",
-  },
-  {
-    name: "Prakash Dahal",
-    img: pd,
-    position: "AI Engineer",
-    description:
-      "Hi, I am Prakash. I am working as a Data Scientist for more than a year. My primary task is to collect and handle large volume of data and to ensure the high quality of data by different methods of data validation and preprocessing techniques.",
-  },
-  // {
-  //   name: "Bigyan Subedi",
-  //   img: niten,
-  //   position: "AI Engineer",
-  //   description:
-  //     "Hi, I am Bigyan. I am working as AI Engineer at ASMII for more than a year. I have been carrying out AI model development and research  work for the company",
-  // },
-  {
-    name: "Roshan Shrestha",
-    img: roshan,
-    position: "AI Engineer",
-    description:
-      "Hey, I am Roshan Shrestha. I hold a BSC (Hons) degree in computer science. I am working as a Data Scientist at ASMII for more than a year now. I enjoy playing football with my buddies on weekends.",
-  },
-  {
-    name: "Simon Tamang",
-    img: simon,
-    position: "UI/UX Designer",
-    description:
-      "Hi, this is Simon, who works as a UI/UX designer in ASMI with more than three years of experience. In my free time, I like to listen to music.",
-  },
-  // {
-  //   name: "Ram Krishna Lamichhane",
-  //   img: niten,
-  //   position: "Developer",
-  //   description: "",
-  // },
-  {
-    name: "Sangam Man Buddhacharya",
-    img: san,
-    position: "AI Engineer",
-    description:
-      "Hi! I'm 'San'. I have been working for a year as an AI Engineer, I am passionate and dedicated learner, willing to solve real-world problems using data, computer vision, and machine learning models. I design AI models and implement different computer vision algorithms to meet the requirements of the projects. Besides my career, my hobbies are flying drones, playing guitar, listening music, reading books, and so on. At last, I do the work I do because I love it.",
-  },
-  // {
-  //   name: "Mohit Thapa",
-  //   img: niten,
-  //   position: "Quality Assurance",
-  //   description: "",
-  // },
-  {
-    name: "Ashish Shrestha",
-    img: ashish,
-    position: "Full Stack - Frontend",
-    description: "",
-  },
-];
-let advisors = [
-  {
-    name: "Ruby Lu",
-    img: ruby,
-    position: ["Advisor", " Global Partnerships[Asia]"],
-  },
-  {
-    name: "Ramzi Ramsey",
-    img: ramzi,
-    position: ["Advisor", " Go-To-Market"],
-  },
-  {
-    name: "Mark Barnard",
-    img: mark,
-    position: ["Advisor", "Agency Partnership[US & Asia]"],
-  },
-  {
-    name: "Rob Garrett",
-    img: rob,
-    position: ["Advisor", "Core Business Growth"],
-  },
-  {
-    name: "Ravi Thomas",
-    img: ravi,
-    position: ["Advisor", "Engineering & Core Business"],
-  },
-];
+
 function TeamMembers() {
-  let founders = [
-    {
-      name: "Sam Thapaliya",
-      img: sam,
-      position: "Co-Founder",
-      title: "President",
-    },
-    {
-      name: "David A. Thomas",
-      img: david,
-      position: "Co-Founder",
-      title: "Head of Operations",
-    },
-    {
-      name: "Sajjan Thapaliya",
-      img: sajjan,
-      position: "Co-Founder",
-      title: "Head of Product",
-    },
-  ];
   let teams = [
     {
       name: "Sagar Adhikari",
@@ -215,13 +86,7 @@ function TeamMembers() {
       description:
         "Hi, I am Prakash. I am working as a Data Scientist for more than a year. My primary task is to collect and handle large volume of data and to ensure the high quality of data by different methods of data validation and preprocessing techniques.",
     },
-    {
-      name: "Bigyan Subedi",
-      img: bigyan,
-      position: "AI Engineer",
-      description:
-        "Hi, I am Bigyan. I am working as AI Engineer at ASMII for more than a year. I have been carrying out AI model development and research  work for the company",
-    },
+
     {
       name: "Roshan Shrestha",
       img: roshan,
@@ -268,7 +133,8 @@ function TeamMembers() {
       name: "Raghib Islam",
       img: raghibi,
       position: "Full Stack - Frontend / Backend",
-      description:"Full Stack engineer with more than two years of experience in frontend & backend technology. At ASMI I work in frontend/backend stuffs.",
+      description:
+        "Full Stack engineer with more than two years of experience in frontend & backend technology. At ASMI I work in frontend/backend stuffs.",
     },
     {
       name: "Jabir Hussain",
